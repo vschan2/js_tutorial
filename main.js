b460f6e1507072711b00c11e952fe25d4495e83d
vschan2 <chanveisiang@hotmail.my>
2018-10-03 21:44:01 +0800

    Events and DOM manipulation

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
 let Phrase = require("mhartl-palindrome");
 
-let string = prompt("Please enter a string for palindrome testing:");
-let phrase = new Phrase(string);
+function palindromeTester(event) {
+  event.preventDefault();
 
-if (phrase.palindrome()) {
-  alert(`"${phrase.content}" is a palindrome!`);
-} else {
-  alert(`"${phrase.content}" is not a palindrome.`)
+  let phrase = new Phrase(event.target.phrase.value);
+  let palindromeResult = document.querySelector("#palindromeResult");
+
+  if (phrase.palindrome()) {
+    palindromeResult.innerHTML = `"<strong>${phrase.content}</strong>" is a palindrome!`;
+  } else {
+    palindromeResult.innerHTML = `"<strong>${phrase.content}</strong>" is a not a palindrome.`;
+  }
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+  let tester = document.querySelector("#palindromeTester");
+  tester.addEventListener("submit", function() {
+    palindromeTester(event);
+  });
+});
